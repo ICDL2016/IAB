492ad7d1f8105dbe731507a258c46891f495a968
timmyru <33581137+timmyru@users.noreply.github.com>
2019-07-17 22:00:47 +0300

    Delete _start.php

--- _start.php
+++
@@ -1,7 +0,0 @@
-<section class="start">
-    <div class="container">
-        <div class="start-title">
-            Начните с демонстрационного курса если вы еще не определились с программой обучения
-        </div>
-    </div>
-</section>
